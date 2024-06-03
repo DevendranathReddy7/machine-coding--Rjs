8a19518cd8ff0d1ca0fef6db1955fcf2e8620384
Devendra <devendranathreddy54@gmail.com>
2024-06-03 21:15:11 +0530

    completing auto suggest

--- src/usecases/AutoSuggestion/AutoSuggestion.js
+++ src/usecases/AutoSuggestion/AutoSuggestion.js
@@ -4,6 +4,7 @@ const AutoSuggestion = (props) => {
   //https://dummyapi.online/api/movies
   const [inputValue, setInputValue] = useState("");
   const [suggestedMovies, setSuggestedMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const searchHandler = (e) => {
     setInputValue(e.target.value);
@@ -11,12 +12,14 @@ const AutoSuggestion = (props) => {
 
   useEffect(() => {
     const getsuggestions = async () => {
+      setIsLoading(true);
       const resp = await fetch("https://dummyapi.online/api/movies");
       const response = await resp.json();
       const suggestedList = response.filter((movie) =>
         movie.movie.includes(inputValue)
       );
       setSuggestedMovies(suggestedList);
+      setIsLoading(false);
     };
     inputValue.length > 2 && getsuggestions();
   }, [inputValue]);
@@ -24,6 +27,24 @@ const AutoSuggestion = (props) => {
   const suggestionClickHandler = (movie) => {
     setInputValue(movie);
     setSuggestedMovies([]);
+  };
+
+  const getresult = (movie, highlight) => {
+    const parts = movie.split(new RegExp(`(${highlight})`, "gi"));
+    console.log(parts);
+    return (
+      <span>
+        {parts.map((part, index) =>
+          part.toLowerCase() === highlight.toLowerCase() ? (
+            <b key={index} style={{ color: "red" }}>
+              {part}
+            </b>
+          ) : (
+            part
+          )
+        )}
+      </span>
+    );
   };
 
   return (
@@ -38,13 +59,14 @@ const AutoSuggestion = (props) => {
       ></input>
       {suggestedMovies.length > 0 && (
         <ul className="auto_suggest_ul">
+          {isLoading && <p style={{ color: "black" }}>Loading....</p>}
           {suggestedMovies.map((mov, index) => (
             <p
               key={index}
               className="auto_suggest_li"
               onClick={() => suggestionClickHandler(mov.movie)}
             >
-              {mov.movie}
+              {getresult(mov.movie, inputValue)}
             </p>
           ))}
         </ul>
